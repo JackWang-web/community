dc6cbfb0b0efd04cf04c9e4a6899f76b7c3974cd
JackWang-Web <260474420@qq.com>
2020-01-17 14:03:24 +0800

    添加了登录

---
+++ src/main/java/life/majiang/community/controller/AuthorizeController.java
@@ -0,0 +1,31 @@
+package life.majiang.community.controller;
+
+import life.majiang.community.dto.AccessTokenDTO;
+import life.majiang.community.dto.GithubUser;
+import life.majiang.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name="code")String code,
+                           @RequestParam(name="state")String state){
+
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id("1001569a21ac06d0b229");
+        accessTokenDTO.setClient_secret("a648fda9f1e7ed82b030f3e0936fa1ff54ae9c54");
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_url("http://localhost/callback");
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+        return "index";
+    }
+}
