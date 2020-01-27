03d89ffaf1f8424f6c77f94e61ee0900ab87c270
JackWang-Web <260474420@qq.com>
2020-01-27 12:22:00 +0800

    add devtools and update bug(textarea)

--- src/main/java/life/majiang/community/controller/IndexController.java
+++ src/main/java/life/majiang/community/controller/IndexController.java
@@ -40,6 +40,7 @@ public class IndexController {
         }
 
         List<QuestionDTO> questionList = questionService.list();
+
         model.addAttribute("questions",questionList);
         return "index";
     }
